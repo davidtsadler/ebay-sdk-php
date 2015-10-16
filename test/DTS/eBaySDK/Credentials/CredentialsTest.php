8a160f92dacf92843ebd65310df5a79f7860b63f
David T. Sadler <davidtsadler@googlemail.com>
2015-10-16 11:28:06 +0100

    update: add missing credentials class test

---
+++ test/DTS/eBaySDK/Credentials/CredentialsTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace DTS\eBaySDK\Credentials\Test;
+
+use DTS\eBaySDK\Credentials\Credentials;
+
+class CredentialsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHasGetters()
+    {
+        $c = new Credentials('111', '222', '333', '444');
+
+        $this->assertEquals('111', $c->getAppId());
+        $this->assertEquals('222', $c->getCertId());
+        $this->assertEquals('333', $c->getDevId());
+        $this->assertEquals('444', $c->getAuthToken());
+    }
+}
