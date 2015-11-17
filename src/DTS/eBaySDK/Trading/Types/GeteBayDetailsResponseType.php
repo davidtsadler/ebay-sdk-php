1be1bed24b7df52dda38c268609ba83a7566afa4
Eno Mullaraj <emullaraj@gmail.com>
2015-11-17 16:41:24 +0100

    Update GeteBayDetailsResponseType.php
    Added Xml root element.

--- src/DTS/eBaySDK/Trading/Types/GeteBayDetailsResponseType.php
+++ src/DTS/eBaySDK/Trading/Types/GeteBayDetailsResponseType.php
@@ -223,6 +223,10 @@ class GeteBayDetailsResponseType extends \DTS\eBaySDK\Trading\Types\AbstractResp
         if (!array_key_exists(__CLASS__, self::$xmlNamespaces)) {
             self::$xmlNamespaces[__CLASS__] = 'urn:ebay:apis:eBLBaseComponents';
         }
+        
+        if (!array_key_exists(__CLASS__, self::$requestXmlRootElementNames)) {
+            self::$requestXmlRootElementNames[__CLASS__] = 'GeteBayDetailsResponse';
+        }
 
         $this->setValues(__CLASS__, $childValues);
     }
