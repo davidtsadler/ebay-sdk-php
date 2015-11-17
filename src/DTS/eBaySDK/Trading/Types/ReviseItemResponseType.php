e147a26af9064943d9ad44957e4c7997cd001759
emullaraj <emullaraj@gmail.com>
2015-11-17 09:50:41 +0100

    Update ReviseItemResponseType.php
    Added XmlRootElement

--- src/DTS/eBaySDK/Trading/Types/ReviseItemResponseType.php
+++ src/DTS/eBaySDK/Trading/Types/ReviseItemResponseType.php
@@ -119,6 +119,10 @@ class ReviseItemResponseType extends \DTS\eBaySDK\Trading\Types\AbstractResponse
             self::$xmlNamespaces[__CLASS__] = 'urn:ebay:apis:eBLBaseComponents';
         }
 
+        if (!array_key_exists(__CLASS__, self::$requestXmlRootElementNames)) {
+            self::$requestXmlRootElementNames[__CLASS__] = 'ReviseItemResponse';
+        }
+        
         $this->setValues(__CLASS__, $childValues);
     }
 }
