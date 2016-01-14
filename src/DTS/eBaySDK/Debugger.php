60a6e9dd8541098105691fcafd68a1441c7a1b41
David T. Sadler <davidtsadler@googlemail.com>
2016-01-14 23:14:07 +0000

    update: scrub credentials in debug messages

--- src/DTS/eBaySDK/Debugger.php
+++ src/DTS/eBaySDK/Debugger.php
@@ -8,18 +8,40 @@ class Debugger
 {
     private $config;
 
+    private static $credentialsStrings = [
+        '/^(X-EBAY-SOA-SECURITY-TOKEN:.*)?$/im' => 'X-EBAY-SOA-SECURITY-TOKEN: SECURITY-TOKEN',
+        '/^(X-EBAY-SOA-SECURITY-APPNAME:.*)?$/im' => 'X-EBAY-SOA-SECURITY-APPNAME: SECURITY-APPNAME',
+        '/^(X-EBAY-API-AFFILIATE-USER-ID:.*)?$/im' => 'X-EBAY-API-AFFILIATE-USER-ID: AFFILIATE-USER-ID',
+        '/^(X-EBAY-API-APP-ID:.*)?$/im' => 'X-EBAY-API-APP-ID: APP-ID',
+        '/^(X-EBAY-API-TRACKING-ID:.*)?$/im' => 'X-EBAY-API-TRACKING-ID: TRACKING-ID',
+        '/^(X-EBAY-API-TRACKING-PARTNER-CODE:.*)?$/im' => 'X-EBAY-API-TRACKING-PARTNER-CODE: TRACKING-PARTNER-CODE',
+        '/^(X-EBAY-API-APP-NAME:.*)?$/im' => 'X-EBAY-API-APP-NAME: APP-NAME',
+        '/^(X-EBAY-API-CERT-NAME:.*)?$/im' => 'X-EBAY-API-CERT-NAME: CERT-NAME',
+        '/^(X-EBAY-API-DEV-NAME:.*)?$/im' => 'X-EBAY-API-DEV-NAME: DEV-NAME ',
+        '/<eBayAuthToken>.*<\/eBayAuthToken>/i' => '<eBayAuthToken>EBAY-AUTH-TOKEN</eBayAuthToken>'
+    ];
+
     /**
      * @param array $config Debug configuration.
      */
     public function __construct($config)
     {
         $this->config = $config + [
-            'logfn' => function ($value) { echo $value; }
+            'logfn' => function ($value) { echo $value; },
+            'scrub_credentials' => true,
+            'credentials_strings' => []
         ];
+
+        $this->config['credentials_strings'] += self::$credentialsStrings;
     }
 
     public function __invoke($info)
     {
+        if ($this->config['scrub_credentials']) {
+            foreach ($this->config['credentials_strings'] as $pattern => $replacement) {
+                $info = preg_replace($pattern, $replacement, $info);
+            }
+        }
         $this->config['logfn']($info);
     }
 }
--- test/DTS/eBaySDK/DebuggerTest.php
+++ test/DTS/eBaySDK/DebuggerTest.php
@@ -16,5 +16,39 @@ class DebuggerTest extends \PHPUnit_Framework_TestCase
 
         $this->assertContains('debug', $str);
     }
+
+    public function testScrubsCredentials()
+    {
+        $msg = <<<EOT
+X-EBAY-SOA-SECURITY-TOKEN: token
+X-EBAY-SOA-SECURITY-APPNAME: appname
+X-EBAY-API-AFFILIATE-USER-ID: userid
+X-EBAY-API-APP-ID: appid
+X-EBAY-API-TRACKING-ID: trackingid
+X-EBAY-API-TRACKING-PARTNER-CODE: partnercode
+X-EBAY-API-APP-NAME: appname
+X-EBAY-API-CERT-NAME: certname
+X-EBAY-API-DEV-NAME: devname
+<eBayAuthToken> authtoken </eBayAuthToken>
+EOT;
+
+        $str = '';
+        $logfn = function ($value) use (&$str) { $str .= $value; };
+
+        $d = new Debugger(['logfn' => $logfn]);
+
+        $d($msg);
+
+        $this->assertNotContains('token', $str);
+        $this->assertNotContains('appname', $str);
+        $this->assertNotContains('userid', $str);
+        $this->assertNotContains('appid', $str);
+        $this->assertNotContains('trackingid', $str);
+        $this->assertNotContains('partnercode', $str);
+        $this->assertNotContains('appname', $str);
+        $this->assertNotContains('certname', $str);
+        $this->assertNotContains('devname', $str);
+        $this->assertNotContains('authtoken', $str);
+    }
 }
 
