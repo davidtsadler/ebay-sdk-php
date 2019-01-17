66af608d0c1e50f332acce172b6e15d7f3e6de29
knaftoli <45397679+knaftoli@users.noreply.github.com>
2019-01-17 12:20:14 +0200

    Update MarketingService.php
    Update Resource URI

--- src/Marketing/Services/MarketingService.php
+++ src/Marketing/Services/MarketingService.php
@@ -31,7 +31,7 @@ class MarketingService extends \DTS\eBaySDK\Marketing\Services\MarketingBaseServ
         ],
         'BulkCreateAdsByListingID' => [
             'method' => 'POST',
-            'resource' => 'campaign/{campaign_id}/bulk_create_ads_by_listing_id',
+            'resource' => 'ad_campaign/{campaign_id}/bulk_create_ads_by_listing_id',
             'responseClass' => '\DTS\eBaySDK\Marketing\Types\BulkCreateAdsByListingIDRestResponse',
             'params' => [
                 'campaign_id' => [
